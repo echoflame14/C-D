edf79cfd6421d79a8eec9f32b7c64c08485dcc6f
echoflame14 <joshpeterson.dev@gmail.com>
2016-06-27 18:24:46 -0600

    Made misc improvements to jade files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-var player = {
-	str: 5,
-	per: 5,
-	agl: 5,
-	cha: 5
+var player = {};
+var init = function() {
+	player = {
+		str: 5,
+		per: 5,
+		agl: 5,
+		cha: 5
+	};
+alert("working");
 };
+alert("hello");
